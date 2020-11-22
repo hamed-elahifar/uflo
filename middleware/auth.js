d603ed7c47ff4d4fd256f2bafb1f4fa14e74f131
Hamed ElahiFar <hamed.elahifar@gmail.com>
2020-11-22 22:17:39 +0330

    remove console.log(token)

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,7 +14,7 @@ module.exports  = async function (req, res, next) {
     let token = req.header('token') ? req.header('token') :   
                 req.body.token      ? req.body.token      : null
 
-    console.log('token',token)
+    // console.log('token',token)
 
     if (!token) return next({status:401,msg:'Access Denied, No Token Provided'});
     
