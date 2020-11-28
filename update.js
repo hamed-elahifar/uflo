d960d398741b8411859afa0a9852c5270027aa1d
Hamed ElahiFar <hamed.elahifar@gmail.com>
2020-11-28 19:18:46 +0330

    update service #4

--- update.js
+++ update.js
@@ -26,10 +26,6 @@ app.all('/:item/:mode?',(req,res)=>{
 
             stderr ? res.send(stderr) : res.send(stdout)
 
-            console.log('err',   err)
-            console.log('stderr',stderr)
-            console.log('stdout',stdout)
-
             if (data[item].pm2name && !err && !stderr && stdout == 'Already up to date.'){
                 exec(`pm2 restart ${data[item].pm2name}`,(err,stdout,stderr) => {
                     if (err)    {console.log(err)}
