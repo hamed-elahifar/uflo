2c4d12417c5a333755b4c2066d5d4291c750220a
Hamed ElahiFar <hamed.elahifar@gmail.com>
2021-08-03 10:29:22 +0430

    add order to frames

--- src/models/frames.js
+++ src/models/frames.js
@@ -1,87 +1,74 @@
 const mongoose     = require('mongoose')
 const {mongoDB}    = require('../startup/mongodb')
 
-const contentSchema = new mongoose.Schema({
-    order:{
-        type:       Number,
-        min:        0
-    },
-    type:{
-        type:       String,
-        enum:       ['description','quote','definition','table','question','function'],
-    },
-    html:           String,
-})
-
 const frameSchema  = new mongoose.Schema({
 
-    title:{
-        type:           String,
-        required:       true
-    },
-    frameType:{
-        type:           String,
-        enum:           ['normal','question']
-    },
+    title:              String,
     frameID:{
         type:           Number,
         // default:        () => Math.random().toString(35).substr(2,10),
         unique:         true,
+    },
+    frameType:{
+        type:           String,
+        // enum:           ['normal','question']
     },
     lobjID:{
         type:           String,
         required:       true,
         ref:            'lobjs'
     },
-    courseID:{
-        type:           String,
-        required:       true,
-        ref:            'courses'
-    },
-    chapterID:{
-        type:           String,
-        required:       true,
-        ref:            'chapters'
-    },
-    lessonID:{
-        type:           String,
-        required:       true,
-        ref:            'lessons'
-    },
-    order:{
-        type:           Number,
-        min:            0
-    },
-    components:         String,
-    voice:              String,
-    sticky:[{
-        sticky:         Boolean,
-        height:         String,
-    }],
+    tags:               String,
+    draggables:         String,
+    order:              Number,
+    // courseID:{
+    //     type:           String,
+    //     required:       true,
+    //     ref:            'courses'
+    // },
+    // chapterID:{
+    //     type:           String,
+    //     required:       true,
+    //     ref:            'chapters'
+    // },
+    // lessonID:{
+    //     type:           String,
+    //     required:       true,
+    //     ref:            'lessons'
+    // },
+    // order:{
+    //     type:           Number,
+    //     min:            0
+    // },
+
+    // sticky:[{
+    //     sticky:         Boolean,
+    //     height:         String,
+    // }],
 
 },{
     timestamps:          true,
     toJSON:              {virtuals:true},
 });
 
-frameSchema.virtual('course',{
-    ref:           'courses',
-    localField:    'courseID',
-    foreignField:  'courseID',
-    justOne:        true,
-});
-frameSchema.virtual('chapter',{
-    ref:           'chapters',
-    localField:    'chapterID',
-    foreignField:  'chapterID',
-    justOne:        true,
-});
-frameSchema.virtual('lesson',{
-    ref:           'lessons',
-    localField:    'lessonID',
-    foreignField:  'lessonID',
-    justOne:        true,
-});
+// frameSchema.virtual('course',{
+//     ref:           'courses',
+//     localField:    'courseID',
+//     foreignField:  'courseID',
+//     justOne:        true,
+// });
+// frameSchema.virtual('chapter',{
+//     ref:           'chapters',
+//     localField:    'chapterID',
+//     foreignField:  'chapterID',
+//     justOne:        true,
+// });
+// frameSchema.virtual('lesson',{
+//     ref:           'lessons',
+//     localField:    'lessonID',
+//     foreignField:  'lessonID',
+//     justOne:        true,
+// });
 frameSchema.virtual('lobj',{
     ref:           'lobjs',
     localField:    'lobjID',
